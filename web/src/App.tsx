697eb1705223976ab10ae58977ec9e53b0632715
Carlos Gallo <carlosasgallo@gmail.com>
2023-01-03 21:35:57 -0300

    layout v1"

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./styles/main.css";
+import logoImg from "./assets/logo-nlw-esports.svg";
 
 interface ButtonProps {
   title: string;
@@ -9,7 +11,43 @@ function Button(props: ButtonProps) {
 }
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center m-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu
+        <span className=" text-transparent bg-nlw-gradient bg-clip-text  ">
+          {" "}
+          duo{" "}
+        </span>
+        está aqui
+      </h1>
+
+      <div className=" grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient"></div>
+        </a>
+        <a href="" className="relative">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="/game-2.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
